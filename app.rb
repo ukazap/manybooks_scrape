621a064ce5f7026f2eb2ede6e9e9cd4146c71ae1
Ukaza Perdana <ukazap@live.com>
2015-08-01 18:47:42 +0700

    Send User-Agent in request header to fool server into thinking that I'm a regular Firefox (actually, Waterfox) user :)

--- app.rb
+++ app.rb
@@ -6,18 +6,21 @@ require './helpers'
 require './extract-manybooks'
 
 $LOG = Logger.new("scraping.log", "monthly")
+$BROWSER = "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0 Waterfox/39.0"
+
 grup = %w[1 a b c d e f g h i j k l m n o p q r s t u v w x y z]
 
 grup.each do |g|
   # mulai dari sini:
   s = 1
   url = "http://manybooks.net/titles.php?alpha=#{g}&s=#{s}"
-  doc = Nokogiri::HTML(open(url))
+  doc = Nokogiri::HTML(open(url, "User-Agent" => $BROWSER))
 
   # loop:
   until sudah_buntu(doc)
+    puts_and_log("Begin ABJAD #{g} HALAMAN #{s}")
+
     doc.css('.smallBookTitle').each do |link|
-      puts_and_log("Begin ABJAD #{g} HALAMAN #{s}")
       source_url = "http://manybooks.net#{link[:href]}"
       extract_manybooks(source_url)
     end
@@ -25,6 +28,6 @@ grup.each do |g|
     # inkremen:
     s += 1
     url = "http://manybooks.net/titles.php?alpha=#{g}&s=#{s}"
-    doc = Nokogiri::HTML(open(url))
+    doc = Nokogiri::HTML(open(url, "User-Agent" => $BROWSER))
   end
 end
