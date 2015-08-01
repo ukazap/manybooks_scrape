28a011bdfd5ec1eff5b69afc7d207ae470ec92e9
Ukaza Perdana <ukazap@live.com>
2015-08-01 18:42:05 +0700

    Main app script

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'open-uri'
+require 'logger'
+require './schema'
+require './helpers'
+require './extract-manybooks'
+
+grup = %w[1 a b c d e f g h i j k l m n o p q r s t u v w x y z]
+
+grup.each do |g|
+  # mulai dari sini:
+  s = 1
+  url = "http://manybooks.net/titles.php?alpha=#{g}&s=#{s}"
+  doc = Nokogiri::HTML(open(url))
+
+  # loop:
+  until sudah_buntu(doc)
+    doc.css('.smallBookTitle').each do |link|
+      source_url = "http://manybooks.net#{link[:href]}"
+      extract_manybooks(source_url)
+    end
+
+    # inkremen:
+    s += 1
+    url = "http://manybooks.net/titles.php?alpha=#{g}&s=#{s}"
+    doc = Nokogiri::HTML(open(url))
+  end
+end
