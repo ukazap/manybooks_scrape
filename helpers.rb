48288d54bb149a7035bfdf3db66f75a4a489dee5
Ukaza Perdana <ukazap@live.com>
2015-08-03 07:39:59 +0700

    refactor puts_and_log, remove sudah_buntu

--- helpers.rb
+++ helpers.rb
@@ -1,15 +1,10 @@
-def sudah_buntu(doc)
-  return (doc.css('a[title="next"]').count == 0)? true : false
-end
-
 def integerize(some_string)
   some_string.gsub(/[^0-9a-z\\s]/i, '')
 end
 
 def puts_and_log(message, type='INFO')
   puts message
-  case type
-  when "ERROR"
+  if type == "ERROR"
     $LOG.error(message)
   else
     $LOG.info(message)
